1d5b77e00d53891e03b65ba4fc1618e07064bd8d
LCYMike <33358967+LCYMike@users.noreply.github.com>
2018-12-17 09:41:49 +0100

    Add's checkpoint

---
+++ Vertical slice - 1/Assets/RotateCheckPoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCheckPoint : MonoBehaviour {
+
+    void Update () {
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
+	}
+}
