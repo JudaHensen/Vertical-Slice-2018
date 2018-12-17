f16b784a0db5e3a2fe66c9de97c6643bacaf4a5f
LCYMike <33358967+LCYMike@users.noreply.github.com>
2018-12-17 09:54:49 +0100

    Add's collider to checkpoint on start

--- Vertical slice - 1/Assets/RotateCheckPoint.cs
+++ Vertical slice - 1/Assets/RotateCheckPoint.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class RotateCheckPoint : MonoBehaviour {
 
+    private BoxCollider col;
+
+    void Start()
+    {
+        col = gameObject.AddComponent<BoxCollider>();
+    }
+
     void Update () {
         transform.rotation = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
 	}
