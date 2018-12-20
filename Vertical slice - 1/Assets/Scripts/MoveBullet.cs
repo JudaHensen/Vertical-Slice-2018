db8ce72fcf011d66c9206a358d7a99f24b4b413a
JudaHensen <24311@ma-web.nl>
2018-12-20 08:23:11 +0100

    Added movebullet

---
+++ Vertical slice - 1/Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour {
+
+    private Transform trans;
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        trans = GetComponent<Transform>();
+    }
+	
+	void Update () {
+
+        trans.position += transform.forward * Time.deltaTime * speed;
+	}
+}
